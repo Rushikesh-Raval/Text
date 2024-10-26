e1811d70eaf10f91907fd99ea5c2e4bc75f1c1af
Rushikesh Raval <147236534+Rushikesh-Raval@users.noreply.github.com>
2024-10-26 22:41:14 +0530

    fixed routing proble

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ const __dirname1 = path.resolve()
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname1,"../frontend/build")))
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname1, "../frontend/build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "../frontend/build", "index.html"));
+  });
 }else{
   app.get("/",(req,res)=>{
     res.send("API Is Running Successfully")
